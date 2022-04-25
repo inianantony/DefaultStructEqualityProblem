6b6d9df52640513f861a1b962cdeef7981f6a013
Antony Inian <ainian@faralloncapital.com>
2022-04-25 09:58:07 +0800

    Initial proj

---
+++ DefaultStructEqualityProblem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DefaultStructEqualityProblem
+{
+  public struct SummaryKey
+  {
+
+  }
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
