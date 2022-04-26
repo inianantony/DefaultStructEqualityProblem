ec2c206d827794bfa5999dc983509951cee95001
Antony Inian <ainian@faralloncapital.com>
2022-04-26 12:55:15 +0800

    Use resharper's autosuggested IEquatable implementation to generate unique hashes

--- DefaultStructEqualityProblem/CustomKeys.cs
+++ DefaultStructEqualityProblem/CustomKeys.cs
@@ -29,32 +29,76 @@ namespace DefaultStructEqualityProblem
       UserGroup = userGroup;
     }
 
-    public bool Equals(SummaryKeyEquatable aKey) => (aKey != null) && (ReferenceEquals(this, aKey) || (aKey.PartitionType == PartitionType && aKey.MarketType == MarketType && aKey.UserGroup == UserGroup));
-
-    public override bool Equals(object aCompareToKey)
+    public bool Equals(SummaryKeyEquatable other)
     {
-      if (aCompareToKey == null)
-        return false;
-
-      if (aCompareToKey is SummaryKeyEquatable key)
-        return Equals(key);
-
-      throw new ArgumentException("You can only use a SummaryKeyEquatable type.", nameof(aCompareToKey));
+      if (ReferenceEquals(null, other)) return false;
+      if (ReferenceEquals(this, other)) return true;
+      return PartitionType == other.PartitionType && MarketType == other.MarketType && UserGroup == other.UserGroup;
     }
 
-    public override int GetHashCode() => PartitionType.GetHashCode() + MarketType.GetHashCode() + UserGroup.GetHashCode();
-
-    public static bool operator ==(SummaryKeyEquatable aOne, SummaryKeyEquatable aTwo)
+    public override bool Equals(object obj)
     {
-      if (((object)aOne == null) && ((object)aTwo == null))
-        return true;
-
-      if (((object)aOne == null) || ((object)aTwo == null))
-        return false;
-
-      return aOne.Equals(aTwo);
+      if (ReferenceEquals(null, obj)) return false;
+      if (ReferenceEquals(this, obj)) return true;
+      if (obj.GetType() != this.GetType()) return false;
+      return Equals((SummaryKeyEquatable) obj);
     }
 
-    public static bool operator !=(SummaryKeyEquatable aOne, SummaryKeyEquatable aTwo) => !(aOne == aTwo);
+    public override int GetHashCode()
+    {
+      return HashCode.Combine(PartitionType, MarketType, UserGroup);
+    }
+
+    public static bool operator ==(SummaryKeyEquatable left, SummaryKeyEquatable right)
+    {
+      return Equals(left, right);
+    }
+
+    public static bool operator !=(SummaryKeyEquatable left, SummaryKeyEquatable right)
+    {
+      return !Equals(left, right);
+    }
   }
+
+  //public class SummaryKeyEquatable : IEquatable<SummaryKeyEquatable>
+  //{
+  //  public string PartitionType { get; }
+  //  public string MarketType { get; }
+  //  public string UserGroup { get; }
+
+  //  public SummaryKeyEquatable(string partitionType, string marketType, string userGroup)
+  //  {
+  //    PartitionType = partitionType;
+  //    MarketType = marketType;
+  //    UserGroup = userGroup;
+  //  }
+
+  //  public bool Equals(SummaryKeyEquatable aKey) => (aKey != null) && (ReferenceEquals(this, aKey) || (aKey.PartitionType == PartitionType && aKey.MarketType == MarketType && aKey.UserGroup == UserGroup));
+
+  //  public override bool Equals(object aCompareToKey)
+  //  {
+  //    if (aCompareToKey == null)
+  //      return false;
+
+  //    if (aCompareToKey is SummaryKeyEquatable key)
+  //      return Equals(key);
+
+  //    throw new ArgumentException("You can only use a SummaryKeyEquatable type.", nameof(aCompareToKey));
+  //  }
+
+  //  public override int GetHashCode() => PartitionType.GetHashCode() + MarketType.GetHashCode() + UserGroup.GetHashCode();
+
+  //  public static bool operator ==(SummaryKeyEquatable aOne, SummaryKeyEquatable aTwo)
+  //  {
+  //    if (((object)aOne == null) && ((object)aTwo == null))
+  //      return true;
+
+  //    if (((object)aOne == null) || ((object)aTwo == null))
+  //      return false;
+
+  //    return aOne.Equals(aTwo);
+  //  }
+
+  //  public static bool operator !=(SummaryKeyEquatable aOne, SummaryKeyEquatable aTwo) => !(aOne == aTwo);
+  //}
 }
